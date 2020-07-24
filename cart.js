45a2ee14748500ac7947dbc1f0faee0dc399a5b5
Anik Sarker <aniksarker1612@gmail.com>
2020-07-24 22:19:23 +0800

    almost done but can't use function to reduce repeatation work. have to do it :)

--- cart.js
+++ cart.js
@@ -1,28 +1,50 @@
-const price = document.getElementById('spanText').innerText;
-const priceNum = parseInt(price);
+// plusBtn 
+var plusBtn = document.querySelectorAll('.plusBtn');
+var quantityText = document.querySelectorAll('.quantityText');
 
-const itemAdd = document.getElementById('addBtn');
-itemAdd.addEventListener('click', function () {
-    let itemCount = document.getElementById('addItemText').value;
-    let count = parseInt(itemCount);
-    document.getElementById('addItemText').value = ++count;
-    let inTotal = count * priceNum;
-    document.getElementById('spanText').innerText = inTotal;
-})
+// minusBtn
+var minusBtn = document.querySelectorAll('.minusBtn');
 
-const itemMinus = document.getElementById('minusBtn')
-itemMinus.addEventListener('click', function () {
-    let itemCount = document.getElementById('addItemText').value;
-    let count = parseInt(itemCount);
-    if (count > 0) {
-        document.getElementById('addItemText').value = --count;
-        let inTotal = count * priceNum;
-        document.getElementById('spanText').innerText = inTotal;
-    }
-
-})
+//spanText 
+var spanText = document.querySelectorAll('.spanText');
 
 
+// plusbtn eventListener
+for (let i = 0; i < plusBtn.length; i++) {
+    const button = plusBtn[i];
+    button.addEventListener('click', function () {
+        if(quantityText[i].value == "" || quantityText[i].value == NaN){
+            quantityText[i].value = "0"; 
+        }
+        var quantityNum = parseInt(quantityText[i].value);
+        quantityText[i].value = ++quantityNum;
+        //var price = parseInt(spanText[i].innerText);
+        var price = [1219, 59];
+        var totalPrice = (quantityNum * price[i]);
+        spanText[i].innerText = totalPrice;
+        subTotal = parseInt(spanText[0].innerText) + parseInt(spanText[1].innerText);
+        document.getElementById('subTotalSpan').innerText = subTotal;
+    })
+}
+
+//minusBtn eventListener
+for (let i = 0; i < minusBtn.length; i++) {
+    const button1 = minusBtn[i];
+    button1.addEventListener('click', function () {
+        var quantityNum = parseInt(document.querySelectorAll('.quantityText')[i].value);
+        if(quantityNum <1){
+            quantityNum = 1;
+        }
+        quantityText[i].value = --quantityNum;
+        var price = [1219, 59];
+        var totalPrice = (quantityNum * price[i]);
+        spanText[i].innerText = totalPrice;
+        subTotal = parseInt(spanText[0].innerText) + parseInt(spanText[1].innerText);
+        document.getElementById('subTotalSpan').innerText = subTotal;
+    })
+}
+
+// item-remove function
 var removeItem = document.getElementsByClassName('remove-item');
 for (let i = 0; i < removeItem.length; i++) {
     var element = removeItem[i];
